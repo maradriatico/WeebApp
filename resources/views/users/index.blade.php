38d4e5cf986697f7bbc5c944d4cdefa9e9b65364
Adrian Roldan Rodriguez <adrian.roldan.rodriguez@iesdonana.org>
2022-06-10 22:33:52 +0200

    pov user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,6 +2,13 @@
     <div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
+            <div class="bg-white">
+
+                @livewire('perfil', ['perfil'=>$perfil])
+
+
+            </div>
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex">
                 <li class="flex ">
                     <ul>
