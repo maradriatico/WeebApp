9f44d33b6e5cad62c2c27a1d8e34320ea57c69cd
Adrian Roldan Rodriguez <adrian.roldan.rodriguez@iesdonana.org>
2022-06-13 03:03:57 +0200

    boton back

--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -91,5 +91,7 @@
         </div>
         <button type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Enviar</button>
+        <a href="/"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Atrás</a>
     </form>
 </x-productos>
