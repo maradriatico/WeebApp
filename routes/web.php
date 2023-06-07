08cb7b5d55e74036ab8c0d08490f2105796dbd91
Adrian <adrian.roldan.rodriguez@iesdonana.org>
2023-06-07 19:29:31 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
