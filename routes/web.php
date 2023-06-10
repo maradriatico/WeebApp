ed261fc472b0f6f7a6661016c863fd73d062585e
Adrian <adrian.roldan.rodriguez@iesdonana.org>
2023-06-10 21:02:58 +0200

    ruta principal

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('register');
 });
 
 Route::get('/dashboard', function () {
